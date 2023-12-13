976cbb4122c4b066e2f4e4ff033f7efb9f656fd3
Simon Cozens <simon@simon-cozens.org>
2023-12-13 12:28:52 +0000

    Use tqdm for progress, fixes #6

--- lib/youseedee/__init__.py
+++ lib/youseedee/__init__.py
@@ -8,6 +8,7 @@ import csv
 import bisect
 
 from filelock import FileLock
+from tqdm import tqdm
 
 def bisect_key(haystack, needle, key=None):
   if sys.version_info[0:2] >= (3, 10):
@@ -33,22 +34,13 @@ def ensure_files():
   lock = FileLock(zip_path + '.lock')
   with lock:
     if not os.path.isfile(zip_path):
-      with open(zip_path, "wb") as f:
-        print("Downloading Unicode Character Database...")
-        response = requests.get(UCD_URL, stream=True)
-        total_length = response.headers.get('content-length')
-
-        if total_length is None: # no content length header
-          f.write(response.content)
-        else:
-          dl = 0
-          total_length = int(total_length)
-          for data in response.iter_content(chunk_size=4096):
-            dl += len(data)
-            f.write(data)
-            done = int(50 * dl / total_length)
-            sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )
-            sys.stdout.flush()
+      response = requests.get(UCD_URL, stream=True)
+      with tqdm.wrapattr(open(zip_path, "wb"), "write",
+                   miniters=1,
+                   total=int(response.headers.get('content-length', 0))) as fout:
+        for chunk in response.iter_content(chunk_size=4096):
+                fout.write(chunk)
+        fout.close()
 
   with zipfile.ZipFile(zip_path, 'r') as zip_ref:
     zip_ref.extractall(ucd_dir())
